738b7e36b35cbb1c16b056a24bfcef6ee3b8ffa8
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2023-09-04 21:30:47 +0300

    Initial commit

---
+++ src/main/java/ru/mikhailov/coffeemaker/CoffeeMakerApplication.java
@@ -0,0 +1,13 @@
+package ru.mikhailov.coffeemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mikhailov/coffeemaker/CoffeeMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikhailov.coffeemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
