281b7193008e6d608f487bff876023c8d9dc636b
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2023-09-05 15:16:39 +0300

    Creating model

--- src/main/java/ru/mikhailov/coffeemaker/model/Coffee.java
+++ src/main/java/ru/mikhailov/coffeemaker/model/Coffee.java
@@ -1,4 +1,31 @@
 package ru.mikhailov.coffeemaker.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table (name = Coffee.TABLE_COFFEE, schema = Coffee.SCHEMA_TABLE)
 public class Coffee {
+
+    public static final String TABLE_COFFEE= "coffee";
+    public static final String SCHEMA_TABLE = "public";
+    public static final String COFFEE_ID = "coffee_id";
+    public static final String ROLE_NAME = "coffee_name";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = COFFEE_ID)
+    Long id;
+
+    @Column(name = ROLE_NAME)
+    String name;
 }
